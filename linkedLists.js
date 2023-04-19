253278122460425a5f697348adc372e9c800dfb2
Suman Das <sum4ndas@gmail.com>
2023-04-19 12:46:26 +0530

    append() adds new node with value at the end

--- linkedLists.js
+++ linkedLists.js
@@ -1,37 +1,36 @@
 const LinkedList = (nextAddress = null) => {
-  // let nextAddress = null;
-  // let list = { nextAddress };
-  return { nextAddress };
+  let data = "head";
+  return { data, nextAddress };
 };
 
 let list = LinkedList();
-
-console.log(list.nextAddress);
 
 const Node = (data = null, nextAddress = null) => {
   return { data, nextAddress };
 };
 
+// adds new node containing value at the end of the list
 function append(value) {
+  if (value == null) return;
+
   let obj = Node(value);
 
+  // if list is empty, add the object next to head.
+  // else add the object at the end
   if (list.nextAddress == null) {
     list = LinkedList(obj);
   } else {
     let tmp = list.nextAddress;
-    console.log(tmp);
-    console.log(tmp.nextAddress);
     while (tmp.nextAddress != null) {
       tmp = tmp.nextAddress;
+      // console.log(tmp, tmp.nextAddress);
     }
     tmp.nextAddress = obj;
-    // console.log(tmp.nextAddress);
   }
-
-  console.log(list);
 }
 
 append("a");
 append("b");
 append("c");
 append("d");
+console.log(list);
