c59091cf2cca47d598c9a0e9720bcf6e4c840a7b
Suman Das <sum4ndas@gmail.com>
2023-04-21 14:20:42 +0530

    Add prepend() inside LinkedList factory

--- linkedLists.js
+++ linkedLists.js
@@ -1,4 +1,4 @@
-const NodeItem = (data = null, next = null) => {
+const Node = (data = null, next = null) => {
   return { data, next };
 };
 
@@ -11,7 +11,7 @@ const LinkedList = (next = null) => {
   const append = (value) => {
     if (value == null) return;
 
-    let obj = NodeItem(value);
+    let obj = Node(value);
 
     if (linkedList.next == null) {
       linkedList.next = obj;
@@ -24,7 +24,21 @@ const LinkedList = (next = null) => {
     }
   };
 
-  return { linkedList, append };
+  // add new node containing value to the start of the list
+  const prepend = (value) => {
+    if (value == null) return;
+
+    let obj = Node(value);
+
+    if (linkedList.next == null) {
+      linkedList.next = obj;
+    } else {
+      obj.next = linkedList.next;
+      linkedList.next = obj;
+    }
+  };
+
+  return { linkedList, append, prepend };
 };
 
 let list = LinkedList();
@@ -32,56 +46,9 @@ list.append("a");
 list.append("b");
 list.append("c");
 console.log(list.linkedList);
-
-// adds new node containing value to the end of the list
-function append(value) {
-  if (value == null) return;
-
-  let obj = Node(value);
-
-  // if list is empty, add the object next to head.
-  // else add the object at the end
-  if (list.nextAddress == null) {
-    list = LinkedList(obj);
-  } else {
-    let tmp = list.nextAddress;
-    while (tmp.nextAddress != null) {
-      tmp = tmp.nextAddress;
-      // console.log(tmp, tmp.nextAddress);
-    }
-    tmp.nextAddress = obj;
-  }
-}
-
-// append("a");
-// append("b");
-// append("c");
-// append("d");
-// console.log("List after appending: ");
-// console.log(list);
-
-// add new node containing value to the start of the list
-function prepend(value) {
-  if (value == null) return;
-
-  let obj = Node(value);
-
-  // if list is empty, just add the object to nextAddress
-  // else change the object's pointer to old head node and
-  // make it new head node.
-  if (list.nextAddress == null) {
-    list.nextAddress = obj;
-  } else {
-    obj.nextAddress = list.nextAddress;
-    list.nextAddress = obj;
-  }
-}
-// console.log();
-// console.log("List after prepending: ");
-// prepend("e");
-// prepend("f");
-
-// console.log(list);
+console.log();
+list.prepend("z");
+console.log(list.linkedList);
 
 // return total number of nodes in the list
 function size() {
